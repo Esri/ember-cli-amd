6652675de43ff990e4cf95147f949fe5b4bf9a2e
Frederic Aubry <faubry@esri.com>
2019-11-09 16:02:03 -0800

    extracted parts into modules

--- index.js
+++ index.js
@@ -14,33 +14,9 @@
 
 const fs = require('fs');
 const path = require('path');
-const cheerio = require('cheerio');
-const _ = require('lodash');
-const beautify_js = require('js-beautify');
-const beautify_html = require('js-beautify').html;
 
 const ReplaceRequireAndDefineFilter = require('./lib/replace-require-and-define-filter');
-
-// The root of the project
-let root;
-
-// For contiinuous build, we need to cache a series of properties
-var indexHtmlCache = {
-  app: {
-    modulesAsString: '',
-    startScript: '',
-    startFileName: ''
-  },
-  test: {
-    modulesAsString: '',
-    startScript: '',
-    startFileName: ''
-  }
-};
-
-// Template used to manufacture the start script
-const startTemplate = _.template(fs.readFileSync(path.join(__dirname, 'start-template.txt'), 'utf8'));
-
+const convertIndexToAmd = require('./lib/convert-index-to-amd');
 
 module.exports = {
 
@@ -48,12 +24,13 @@ module.exports = {
 
   externalAmdModules: new Set(),
 
-  included: function (app) {
-    // Note: this functionis only called once even if using ember build --watch or ember serve
+  included(app) {
+    // Note: this function is only called once even if using ember build --watch or ember serve
 
     // This is the entry point for this addon. We will collect the amd definitions from the ember-cli-build.js and
-    // we will build the list off the amd modules usedby the application.
-    root = app.project.root;
+    // we will build the list off the amd modules used by the application, replace define and require function calls
+    // in the js files and modify the index.html to load AMD loader first and all the external AMD modules before
+    // loading the vendor and app files.
 
     // This addon relies on an 'amd' options in the ember-cli-build.js file
     if (!app.options.amd) {
@@ -79,10 +56,10 @@ module.exports = {
     }
   },
 
-  postprocessTree: function (type, tree) {
+  postprocessTree(type, tree) {
     // Note: this function will be called once during the continuous builds. However, the tree returned will be directly manipulated.
     // It means that the de-requireing will be going on.
-    if (type !== 'all') {
+    if (type !== 'all' || !this.app.options.amd) {
       return tree;
     }
 
@@ -95,160 +72,13 @@ module.exports = {
     });
   },
 
-  postBuild: function (result) {
-
-    if (!this.app.options.amd) {
-      return;
-    }
+  postBuild(result) {
 
     // When ember build --watch or ember serve are used, this function will be called over and over
-    // as a user updates code. We need to figure what we have to build or copy.
+    // as a user updates code.
 
-    // Get the modules information
-    const moduleInfos = this.buildModuleInfos();
-
-    // There are two index files to deal with, the app index file and the test index file.
-    // We need to convert them from ember style to amd style.
-    // Amd style is made of 3 steps:
-    // - amd configuration (optional), controlled by the his.app.options.amd.configPath
-    // - loader: could be based on local build or from cdn
-    // - start of the app: load the amd modules used by the app and boorstrap the app
-
-    // Handle the amd config
-    var amdConfigScript;
-    if (this.app.options.amd.configPath) {
-      amdConfigScript = fs.readFileSync(path.join(root, this.app.options.amd.configPath), 'utf8');
-    }
-
-    // Rebuild the app index files
-    this.indexBuilder({
-      directory: result.directory,
-      indexFile: this.app.options.outputPaths.app.html,
-      indexHtmlCache: indexHtmlCache.app,
-      amdConfigScript,
-      startSrc: 'amd-start',
-      moduleInfos
-    });
-
-    // Rebuild the test index file
-    this.indexBuilder({
-      directory: result.directory,
-      indexFile: 'tests/index.html',
-      indexHtmlCache: indexHtmlCache.test,
-      amdConfigScript,
-      startSrc: 'amd-test-start',
-      moduleInfos
-    });
-  },
-
-  indexBuilder: function (config) {
-    // If the current index html is not the same as teh one we built, it means
-    // that another extension must have forced to regenerate the index html or
-    // this is the first time this extension is running
-    var indexPath = path.join(config.directory, config.indexFile);
-
-    var indexHtml;
-    try {
-      indexHtml = fs.readFileSync(indexPath, 'utf8');
-    } catch (e) {
-      // no index file, we are done.
-      return null;
-    }
-
-    // Check if we have to continue
-    // - If the index already contains the AMD loader
-    // - if the list of modules is still the same
-    const cheerioQuery = cheerio.load(indexHtml);
-    const amdScriptElements = cheerioQuery('script[data-amd]')
-    if (amdScriptElements.length === 1 && config.indexHtmlCache.modulesAsString === config.moduleInfos.names) {
-      return config.indexHtmlCache;
-    }
-
-    // Get the collection of scripts
-    // Scripts that have a 'src' will be loaded by AMD
-    // Scripts that have a body will be assembled into a post loading file and loaded at the end of the AMD loading process
-    var scriptElements = cheerioQuery('body > script');
-    var scriptsToLoad = [];
-    var scriptsToPostExecute = [];
-    scriptElements.each(function () {
-      if (cheerioQuery(this).attr('src')) {
-        scriptsToLoad.push(`"${cheerioQuery(this).attr('src')}"`)
-      } else {
-        scriptsToPostExecute.push(cheerioQuery(this).html());
-      }
-    });
-
-    // Remove the script tags
-    scriptElements.remove();
-
-    // If we have scripts that have to be executed after the AMD load, then serialize them into a file
-    // afterLoading.js and add this file to the list of AMD modules.
-    if (scriptsToPostExecute.length > 0) {
-      var afterLoadingScript = replaceRequireAndDefine(scriptsToPostExecute.join('\n\n'));
-      fs.writeFileSync(path.join(config.directory, 'afterLoading.js'), beautify_js(afterLoadingScript, {
-        indent_size: 2
-      }));
-      scriptsToLoad.push('"/afterLoading.js"');
-    }
-
-    // We have to rebuild this index file.
-    config.indexHtmlCache.modulesAsString = config.moduleInfos.names;
-
-    // Add the amd config
-    var amdScripts = '';
-    if (this.app.options.amd.configPath) {
-      amdScripts += '<script>' + config.amdConfigScript + '</script>';
-    } else if (this.app.options.amd.configScript) {
-      amdScripts += '<script>' + this.app.options.amd.configScript + '</script>';
-    }
-
-    // Add the loader
-    var loaderSrc = this.app.options.amd.loader;
-    amdScripts += `<script src="${loaderSrc}" data-amd="true"></script>`;
-
-    // Add the start scripts
-    var startScript = startTemplate(_.assign(config.moduleInfos, {
-      scripts: scriptsToLoad.join(',')
-    }));
-
-    // Inline the start script
-    amdScripts += '<script>' + startScript + '</script>';
-
-    // Add the scripts to the body
-    cheerioQuery('body').prepend(amdScripts);
-
-    // Beautify the index.html
-    var html = beautify_html(cheerioQuery.html(), {
-      indent_size: 2
-    });
-
-    // Rewrite the index file
-    fs.writeFileSync(indexPath, html);
-
-    return config.indexHtmlCache;
-  },
-
-  buildModuleInfos: function () {
-
-    // Build different arrays representing the modules for the injection in the start script
-    const objs = [];
-    const names = [];
-    const adoptables = [];
-    let index = 0;
-    this.externalAmdModules.forEach(function (amdModule) {
-      objs.push(`mod${index}`);
-      names.push(`'${amdModule}'`);
-      adoptables.push(`{name:'${amdModule}',obj:mod${index}}`);
-      index++;
-    });
-
-    return {
-      names: names.join(','),
-      objects: objs.join(','),
-      adoptables: adoptables.join(','),
-      vendor: path.parse(this.app.options.outputPaths.vendor.js).name
-    };
+    // We can only rebbuild the index after ALL the cli addons have ran. 
+    // We cannot bbuild it during the postPrecessTree!
+    convertIndexToAmd(this.app, result.directory, this.externalAmdModules);
   }
 };
-
-
